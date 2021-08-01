55d4b76591a40f9a36d8bcddb94b52df910d3b1b
Mohd. Asfaq-E-Azam Rifat <mohd.asfaqeazam@gmail.com>
2021-08-01 14:45:00 +0600

    remove redundant files

--- lib/ui/auth/forgot_password/forgot_password_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:go_share/ui/auth/forgot_password/forgot_password_controller.dart';
-
-class ForgotPasswordBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(ForgotPasswordController());
-  }
-}
--- lib/ui/auth/registration/registration_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:go_share/ui/auth/registration/registration_controller.dart';
-
-class RegistrationBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(RegistrationController());
-  }
-}
