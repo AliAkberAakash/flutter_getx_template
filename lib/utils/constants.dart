52ab631174185641adb454b0bafff7cf97d9a542
aliakberaakash <ali852609@gmail.com>
2021-08-05 07:04:42 +0600

    added icons

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,8 +3,20 @@ class AssetConstants {
   static const ic_profile = base_path + "ic_profile.svg";
   static const ic_settings = base_path + "ic_settings.svg";
   static const ic_language = base_path + "ic_language.svg";
-
   static const img_banner_demo = base_path + "img_banner_demo.png";
+
+  static const base_icon_path = "assets/icons/";
+  static const ic_menu_svg = base_icon_path + "ic_menu.svg";
+  static const ic_home_svg = base_icon_path + "ic_home.svg";
+  static const ic_free_busy_list_svg = base_icon_path + "ic_free_busy_list.svg";
+  static const ic_bus_svg = base_icon_path + "ic_bus.svg";
+  static const ic_notification_svg = base_icon_path + "ic_notification.svg";
+  static const ic_profile_svg = base_icon_path + "ic_profile.svg";
+  static const ic_settings_svg = base_icon_path + "ic_settings.svg";
+  static const ic_sign_out_svg = base_icon_path + "ic_sign_out.svg";
+  static const ic_vehicle_list_svg = base_icon_path + "ic_vehicle_list.svg";
+
+
 }
 
 class NetworkConstants{
