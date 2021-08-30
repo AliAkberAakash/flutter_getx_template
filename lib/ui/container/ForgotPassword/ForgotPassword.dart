ab113fc4b1121fb9774c83f577baf2246d935d4b
aliakberaakash <ali852609@gmail.com>
2021-08-30 16:27:13 +0600

    added thank you popup in reset password screen

--- lib/ui/container/ForgotPassword/ForgotPassword.dart
+++ lib/ui/container/ForgotPassword/ForgotPassword.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 import 'package:go_share/base/widget/GSButtonWidget.dart';
 import 'package:go_share/base/widget/GSTextField.dart';
 import 'package:go_share/ui/container/UIConstants/Colors.dart';
 import 'package:go_share/ui/container/UIConstants/GSWidgetStyles.dart';
 import 'package:go_share/ui/container/UIConstants/Strings.dart';
 import 'package:go_share/ui/container/UIConstants/UISizeConstants.dart';
+import 'package:go_share/ui/service_provider/widgets/positive_button.dart';
+import 'package:go_share/utils/constants.dart';
+import 'package:go_share/utils/dimens.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // UI
 class ForgotPassword extends StatefulWidget {
@@ -63,6 +69,9 @@ class _ForgotPasswordState extends State<ForgotPassword> {
           height: GSSizeConstants.padding30,
         ),
         GSButton(
+          onClick: (){
+            showSuccessSheet(context);
+          },
           text: GSStrings.submit,
         ),
         SizedBox(
@@ -70,5 +79,77 @@ class _ForgotPasswordState extends State<ForgotPassword> {
         ),
       ],
     )));
-  } //: Widget
-} //: State
+  }
+
+  void showSuccessSheet(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        builder: (builder) {
+          return new Container(
+            height: 450.0,
+            color: Color(0xFF737373), //could change this to Color(0xFF737373),
+            //so you don't have to change MaterialApp canvasColor
+            child: new Container(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                decoration: new BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: new BorderRadius.only(
+                        topLeft: const Radius.circular(20.0),
+                        topRight: const Radius.circular(20.0))),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SvgPicture.asset(
+                      AssetConstants.successfulIcon,
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: dp30),
+                          child: Text(
+                            "Reset password request sent",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.manrope(
+                              color: GSColors.green_secondary,
+                              fontSize: dp25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            "Please check your email for the code",
+                            style: GoogleFonts.manrope(
+                              color: GSColors.text_secondary,
+                              fontSize: dp14,
+                              fontWeight: FontWeight.normal,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: dp30),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: PositiveButton(
+                              text: "Ok",
+                              onClicked: (){
+                                Get.back();
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+          );
+        });
+  }
+
+}
--- lib/ui/not_logged_in_welcome/sign_in_screen.dart
+++ lib/ui/not_logged_in_welcome/sign_in_screen.dart
@@ -31,138 +31,144 @@ class _Sec4SignInScreenState extends State<NSignInScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         //backgroundColor: GSColors.green_primary,
-        body: Stack(
-      children: [
-        Image.asset(
-          AssetConstants.ic_welcome,
+        body: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          fit: BoxFit.cover,
-        ),
-        SafeArea(
-            child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          //crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              //height: GSSizes.size278h,
-              width: double.infinity,
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.2,
-                  ),
-
-                  //Get start text
-                  Text(
-                    GSStrings.welcome_back,
-                    textAlign: TextAlign.start,
-                    style: GoogleFonts.manrope(
-                      color: white,
-                      fontSize: dp30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-
-                  SizedBox(
-                    height: GSSizeConstants.padding10,
-                  ),
-
-                  // Description test
-                  Container(
-                      width: 256,
-                      child: Center(
-                          child: Text(
-                        "It’s time to rock n role! Let’s\nget started now.",
-                        style: GoogleFonts.manrope(
-                          color: white,
-                          fontSize: dp18,
-                          fontWeight: FontWeight.normal,
-                        ),
-                        textAlign: TextAlign.center,
-                      ))),
-                ],
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                AssetConstants.ic_welcome,
               ),
-            ),
-            SizedBox(
-              height: GSSizeConstants.padding55,
-            ),
-            Sec4TextField(
-              hints: GSStrings.email_field_hints,
-            ),
-            SizedBox(
-              height: GSSizeConstants.padding17,
-            ),
-            Sec4PasswordTextField(),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 5),
-              child: Row(
-                children: [
-                  CustomCheckBox(
-                    value: value,
-                    shouldShowBorder: true,
-                    borderColor: Colors.white,
-                    uncheckedFillColor: Colors.white60,
-                    checkedFillColor: GSColors.green_normal,
-                    borderRadius: 2,
-                    borderWidth: 1,
-                    checkBoxSize: 15,
-                    uncheckedIconColor: Colors.transparent,
-                    onChanged: (val) {
-                      //do your stuff here
-                      setState(() {
-                        value = val;
-                      });
-                    },
-                  ),
-                  Text(
-                    "Keep me signed in",
-                    style: GoogleFonts.manrope(
-                      color: white,
-                      fontSize: dp18,
-                      fontWeight: FontWeight.normal,
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Stack(
+      children: [
+          SafeArea(
+              child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                //height: GSSizes.size278h,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.2,
                     ),
-                  )
-                ],
+
+                    //Get start text
+                    Text(
+                      GSStrings.welcome_back,
+                      textAlign: TextAlign.start,
+                      style: GoogleFonts.manrope(
+                        color: white,
+                        fontSize: dp30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: GSSizeConstants.padding10,
+                    ),
+
+                    // Description test
+                    Container(
+                        width: 256,
+                        child: Center(
+                            child: Text(
+                          "It’s time to rock n role! Let’s\nget started now.",
+                          style: GoogleFonts.manrope(
+                            color: white,
+                            fontSize: dp18,
+                            fontWeight: FontWeight.normal,
+                          ),
+                          textAlign: TextAlign.center,
+                        ))),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              child: GSButton(
-                text: GSStrings.sign_in,
-                onClick: () {
-                  Get.to(NavigationContainer());
+              SizedBox(
+                height: GSSizeConstants.padding55,
+              ),
+              Sec4TextField(
+                hints: GSStrings.email_field_hints,
+              ),
+              SizedBox(
+                height: GSSizeConstants.padding17,
+              ),
+              Sec4PasswordTextField(),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 5),
+                child: Row(
+                  children: [
+                    CustomCheckBox(
+                      value: value,
+                      shouldShowBorder: true,
+                      borderColor: Colors.white,
+                      uncheckedFillColor: Colors.white60,
+                      checkedFillColor: GSColors.green_normal,
+                      borderRadius: 2,
+                      borderWidth: 1,
+                      checkBoxSize: 15,
+                      uncheckedIconColor: Colors.transparent,
+                      onChanged: (val) {
+                        //do your stuff here
+                        setState(() {
+                          value = val;
+                        });
+                      },
+                    ),
+                    Text(
+                      "Keep me signed in",
+                      style: GoogleFonts.manrope(
+                        color: white,
+                        fontSize: dp18,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: GSButton(
+                  text: GSStrings.sign_in,
+                  onClick: () {
+                    Get.to(NavigationContainer());
+                  },
+                ),
+              ),
+              SizedBox(
+                height: GSSizeConstants.padding7,
+              ),
+              //Container(
+              //  margin: EdgeInsets.symmetric(horizontal: 20),
+              //  child: GSButton(
+              //    text: "Sign in as a merchant",
+              //    onClick: () {
+              //      Get.to(NavigationContainer());
+              //    },
+              //  ),
+              //),
+              SizedBox(
+                height: GSSizeConstants.padding7,
+              ),
+              Sec4TextButton(
+                text1: "Forgot Password?",
+                text2: "Reset here",
+                onClicked: (){
+                  Get.to(ForgotPassword());
                 },
               ),
-            ),
-            SizedBox(
-              height: GSSizeConstants.padding7,
-            ),
-            //Container(
-            //  margin: EdgeInsets.symmetric(horizontal: 20),
-            //  child: GSButton(
-            //    text: "Sign in as a merchant",
-            //    onClick: () {
-            //      Get.to(NavigationContainer());
-            //    },
-            //  ),
-            //),
-            SizedBox(
-              height: GSSizeConstants.padding7,
-            ),
-            Sec4TextButton(
-              text1: "Forgot Password?",
-              text2: "Reset here",
-              onClicked: (){
-                Get.to(ForgotPassword());
-              },
-            ),
-            SizedBox(
-              height: GSSizeConstants.padding25,
-            ),
-          ],
-        )),
+              SizedBox(
+                height: GSSizeConstants.padding25,
+              ),
+            ],
+          )),
       ],
-    ));
+    ),
+        ));
   } //: Widget
 } //: State
