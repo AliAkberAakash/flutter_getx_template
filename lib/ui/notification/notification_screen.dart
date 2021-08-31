af4b35f3a860c47b197f8c2889c14fb4697d6be9
aliakberaakash <ali852609@gmail.com>
2021-09-01 04:23:11 +0600

    fixed issue 5

--- lib/ui/notification/notification_screen.dart
+++ lib/ui/notification/notification_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:go_share/ui/container/UIConstants/Colors.dart';
+import 'package:go_share/ui/container/UIConstants/GSWidgetStyles.dart';
+import 'package:go_share/ui/container/UIConstants/Strings.dart';
 import 'package:go_share/utils/colors.dart';
 import 'package:go_share/utils/dimens.dart';
 import 'package:go_share/utils/spacers.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NotificationScreenPage extends StatefulWidget {
   const NotificationScreenPage({Key? key}) : super(key: key);
@@ -35,7 +39,8 @@ class _NotificationScreenPageState extends State<NotificationScreenPage> {
         padding: EdgeInsets.symmetric(horizontal: dp20),
         child: Column(
           children: [
-            VSpacer60(),
+            VSpacer20(),
+            TitleWidget(),
             Expanded(
               child: ListView.builder(
                   itemCount: 10,
@@ -92,4 +97,89 @@ class _NotificationScreenPageState extends State<NotificationScreenPage> {
       ),
     );
   }
+
+
 }
+class TitleWidget extends StatelessWidget {
+  const TitleWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: dp10,
+            vertical: dp10,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "#VA45647",
+                textAlign: TextAlign.start,
+                style: GSTextStyles.make28xw700Style(
+                  color: GSColors.gray_primary,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Start time",
+                          textAlign: TextAlign.start,
+                          style: GoogleFonts.manrope(
+                              color: GSColors.gray_secondary,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          "10:00am",
+                          textAlign: TextAlign.start,
+                          style: GSTextStyles.make16xw400Style(
+                            color: GSColors.gray_secondary,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "End time",
+                          textAlign: TextAlign.start,
+                          style: GoogleFonts.manrope(
+                            color: GSColors.gray_secondary,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          "11:00am",
+                          textAlign: TextAlign.start,
+                          style: GSTextStyles.make16xw400Style(
+                            color: GSColors.gray_secondary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
