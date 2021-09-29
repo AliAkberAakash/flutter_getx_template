ed7364f0dbbcff88c1d5a0fa069dbdb8d9806598
aliakberaakash <ali852609@gmail.com>
2021-09-29 23:57:48 +0600

    added general user info

--- lib/ui/not_logged_in_welcome/profile/profile_screen.dart
+++ lib/ui/not_logged_in_welcome/profile/profile_screen.dart
@@ -8,6 +8,7 @@ import 'package:go_share/ui/common_widgets/text_field_headline.dart';
 import 'package:go_share/ui/common_widgets/text_field_value_widget.dart';
 import 'package:go_share/ui/not_logged_in_welcome/profile/profile_screen_controller.dart';
 import 'package:go_share/utils/colors.dart';
+import 'package:go_share/utils/date_time_utils.dart';
 import 'package:go_share/utils/dimens.dart';
 import 'package:go_share/utils/spacers.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -41,98 +42,107 @@ class _ProfileScreenState extends State<ProfileScreen> {
       }else {
         if (response.data == null) {
           return ErrorScreen();
-        }else return ListView(
-          padding: EdgeInsets.all(dp20),
-          children: [
-            VSpacer20(),
-            VSpacer20(),
-            Row(
+        }else {
+
+          var user = response.data;
+
+          if(user!=null){
+            return ListView(
+              padding: EdgeInsets.all(dp20),
               children: [
-                CircleAvatar(
-                  radius: dp30,
-                  backgroundImage: NetworkImage(
-                    "https://images.unsplash.com/photo-1563306406-e66174fa3787",
-                  ),
-                ),
-                HSpacer20(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                VSpacer20(),
+                VSpacer20(),
+                Row(
                   children: [
-                    Text(
-                      "Violet Norman",
-                      style: GoogleFonts.manrope(
-                        color: darkText,
-                        fontWeight: FontWeight.bold,
-                        fontSize: dp16,
+                    CircleAvatar(
+                      radius: dp30,
+                      backgroundImage: NetworkImage(
+                        user.image ?? "",
                       ),
                     ),
-                    Text(
-                      "bayer_martin@yahoo.com",
-                      style: GoogleFonts.manrope(),
+                    HSpacer20(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          user.name,
+                          style: GoogleFonts.manrope(
+                            color: darkText,
+                            fontWeight: FontWeight.bold,
+                            fontSize: dp16,
+                          ),
+                        ),
+                        Text(
+                          user.email,
+                          style: GoogleFonts.manrope(),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
-            VSpacer40(),
-            Container(
-              height: 1,
-              width: double.maxFinite,
-              color: light_grey,
-            ),
-            VSpacer40(),
-            TextFieldHeadline(headline: "Phone Number"),
-            VSpacer10(),
-            TextFieldValueWidget(headline: '+907079097213'),
-            VSpacer20(),
-            TextFieldHeadline(headline: "Address"),
-            VSpacer10(),
-            TextFieldValueWidget(headline: 'Block 372 Bukit Batok Street 31 #01-372, 650372 Singapore'),
-            VSpacer20(),
-            TextFieldHeadline(headline: "NRIC"),
-            VSpacer10(),
-            TextFieldValueWidget(headline: 'N/A'),
-            VSpacer20(),
-            TextFieldHeadline(headline: "Date of Birth"),
-            VSpacer10(),
-            TextFieldValueWidget(headline: '19 May, 1971'),
-            VSpacer20(),
-            TextFieldHeadline(headline: "Caretaker Info"),
-            VSpacer10(),
-            Row(
-              children: [
-                CircleAvatar(
-                  radius: dp30,
-                  backgroundImage: NetworkImage(
-                    "https://images.unsplash.com/photo-1591258739299-5b65d5cbb235",
-                  ),
+                VSpacer40(),
+                Container(
+                  height: 1,
+                  width: double.maxFinite,
+                  color: light_grey,
                 ),
-                HSpacer20(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                VSpacer40(),
+                TextFieldHeadline(headline: "Phone Number"),
+                VSpacer10(),
+                TextFieldValueWidget(headline: user.phone),
+                VSpacer20(),
+                TextFieldHeadline(headline: "Address"),
+                VSpacer10(),
+                TextFieldValueWidget(headline: user.address),
+                VSpacer20(),
+                TextFieldHeadline(headline: "NRIC"),
+                VSpacer10(),
+                TextFieldValueWidget(headline: user.nric ?? 'N/A'),
+                VSpacer20(),
+                TextFieldHeadline(headline: "Date of Birth"),
+                VSpacer10(),
+                TextFieldValueWidget(headline: user.dateOfBirth != null ? speakDate(user.dateOfBirth!) : "N/A"),
+                VSpacer20(),
+                TextFieldHeadline(headline: "Caretaker Info"),
+                VSpacer10(),
+                Row(
                   children: [
-                    Text(
-                      "John Doe",
-                      style: GoogleFonts.manrope(
-                        color: darkText,
-                        fontWeight: FontWeight.bold,
-                        fontSize: dp16,
+                    CircleAvatar(
+                      radius: dp30,
+                      backgroundImage: NetworkImage(
+                        user.caretakerImage ?? "",
                       ),
                     ),
-                    Text(
-                      "+907079097213",
-                      style: GoogleFonts.manrope(),
+                    HSpacer20(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          user.caretakerName ?? "",
+                          style: GoogleFonts.manrope(
+                            color: darkText,
+                            fontWeight: FontWeight.bold,
+                            fontSize: dp16,
+                          ),
+                        ),
+                        Text(
+                          "+907079097213", //todo fix
+                          style: GoogleFonts.manrope(),
+                        ),
+                      ],
                     ),
                   ],
                 ),
+                VSpacer20(),
+                OutlinedMaterialButton(onClick: (){
+                  Get.to(EditProfileScreen());
+                }, text: "Update My Profile"),
               ],
-            ),
-            VSpacer20(),
-            OutlinedMaterialButton(onClick: (){
-              Get.to(EditProfileScreen());
-            }, text: "Update My Profile"),
-          ],
-        );
+            );
+          }else{
+            return ErrorScreen();
+          }
+        }
       }
     });
   }
