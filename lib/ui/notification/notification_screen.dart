5eb7021f3b7c71f4bf0b4999f71e7205a025887f
ALIAKBERAAKASH <ali852609@gmail.com>
2021-11-30 01:49:20 +0600

    finished child functionality

--- lib/ui/notification/notification_screen.dart
+++ lib/ui/notification/notification_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:go_share/data/models/driver/child_list_response.dart';
 import 'package:go_share/ui/container/UIConstants/Colors.dart';
 import 'package:go_share/ui/container/UIConstants/GSWidgetStyles.dart';
 import 'package:go_share/ui/container/UIConstants/Strings.dart';
@@ -51,61 +52,135 @@ class _NotificationScreenPageState extends State<NotificationScreenPage> {
                   child: ListView.builder(
                       itemCount: childList.length,
                       itemBuilder: (BuildContext context, int index) {
-                        return GestureDetector(
-                          onTap: () {
-                            //todo
-                          },
-                          child: Container(
-                            height: dp80,
-                            width: wp,
-                            color: /*isClicked[index] ?*/ gradientDark/* : light_grey_white*/,
-                            padding: EdgeInsets.symmetric(horizontal: dp10),
-                            margin: EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      'Violet Norman',
-                                      style: TextStyle(
-                                          color: /*isClicked[index] ? white :*/ black,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Text(
-                                      'Block 372 Bukit Batok Street 31.',
-                                      style: TextStyle(
-                                          color: /*isClicked[index] ? white :*/ grey,
-                                          fontSize: dp12),
-                                    )
-                                  ],
-                                ),
-                                CircleAvatar(
-                                  radius: dp20,
-                                  backgroundColor: /*isClicked[index] ? primaryDark :*/ greyBorder,
-                                  child: Icon(
-                                    Icons.check,
-                                    color: /*isClicked[index] ? white :*/ black,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+                        return ChildWidget(
+                          key: new UniqueKey(),
+                          childData: childList[index],
+                          notificationController: notificationController,
                         );
                       }),
                 );
-              }else return Container();
+              }else return Expanded(
+                child: Container(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              );
             }),
           ],
         ),
       ),
     );
   }
-
-
+  
 }
+
+class ChildWidget extends StatefulWidget {
+  
+  final Datum childData;
+  final NotificationController notificationController;
+  
+  const ChildWidget({Key? key, required this.childData, required this.notificationController}) : super(key: key);
+
+  @override
+  _ChildWidgetState createState() => _ChildWidgetState(childData, notificationController);
+}
+
+class _ChildWidgetState extends State<ChildWidget> {
+
+  final Datum data;
+  final NotificationController notificationController;
+
+  bool isExpanded = false;
+
+  _ChildWidgetState(this.data, this.notificationController);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      color: data.isFinished==1 ? gradientDark : light_grey_white,
+      padding: EdgeInsets.symmetric(horizontal: dp10, vertical: dp10),
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: InkWell(
+                  onTap: (){
+                    setState(() {
+                      isExpanded = !isExpanded;
+                    });
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "#${data.bookingId}",
+                        style: TextStyle(
+                          color:data.isFinished==1 ? white : black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      // Text(
+                      //   "${data.}",
+                      //   style: TextStyle(
+                      //       color:data.isFinished==1 ? white : grey,
+                      //       fontSize: dp12),
+                      // )
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: (){
+                  if(data.isFinished == 0)
+                    notificationController.updateChild(data.bookingTravelId);
+                },
+                child: CircleAvatar(
+                  radius: dp20,
+                  backgroundColor:data.isFinished==1 ? primaryDark : greyBorder,
+                  child: Icon(
+                    Icons.check,
+                    color:data.isFinished==1 ? white : black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          isExpanded ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Child List",
+                textAlign: TextAlign.start,
+                style: GoogleFonts.poppins(
+                  color:data.isFinished==1 ? white : black,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              for(var child in data.children!)
+                Text(
+                  "${child.name}",
+                  textAlign: TextAlign.start,
+                  style: GoogleFonts.poppins(
+                    color:data.isFinished==1 ? white : black,
+                  ),
+                )
+            ],
+          ) :  Container(),
+        ],
+      ),
+    );
+  }
+}
+
+
 class TitleWidget extends StatelessWidget {
   const TitleWidget({
     Key? key,
